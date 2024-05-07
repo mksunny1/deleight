d06fee3d644c597921184b49aa74e7f4d3457f24
markersunny <166430764+markersunny@users.noreply.github.com>
2024-05-07 12:22:10 +0100

    Modified deployment pipeline and added dist to .gitignore

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,51 +1,75 @@
 import dts from "rollup-plugin-dts";
 import copy from "rollup-plugin-copy";
+import terser from "@rollup/plugin-terser";
 // import typescript from '@rollup/plugin-typescript';
 
 import { writeFile, mkdir } from "fs/promises";
 
-function createCommonJsPackage() {
-  const pkg = { type: "commonjs" };
-  return {
-    name: "cjs-package",
-    buildEnd: async () => {
-      await mkdir("./dist/cjs", { recursive: true });
-      await writeFile("./dist/cjs/package.json", JSON.stringify(pkg, null, 2));
-    },
-  };
+function createPackage(src) {
+    const pkg = { types: `./${src}.d.ts` };
+    return {
+        name: `${src}`,
+        buildEnd: async () => {
+            await mkdir(`dist/${src}`, { recursive: true });
+            await writeFile(`./dist/${src}/package.json`, JSON.stringify(pkg, null, 2));
+        },
+    };
 }
 
-export default [
-  {
-    input: [
-      "./src/domitory.js",
-      "./src/eventivity.js",
-      "./src/appliance.js",
-      "./src/actribute.js",
-      "./src/apriori.js",
-      "./src/generational.js",
-      "./src/sophistry.js",
-      "./src/onetomany.js",
-      "./src/withy.js",
-    ],
-    plugins: [
-      copy({
-        targets: [{ src: "./package.json", dest: "dist" }],
-      }),
-      createCommonJsPackage(),
-      // terser()
-    ],
-    output: [
-      { format: "es", dir: "./dist/esm" },
-      { format: "cjs", dir: "./dist/cjs" },
-    ],
-  },
-  {
-    input: "./src/deleight.ts",
-    plugins: [dts()],
-    output: {
-      format: "es",
-      file: "./dist/deleight.d.ts",
-    },
-  },
+function createCommonJsPackage(src) {
+    const pkg = { type: "commonjs" };
+    return {
+        name: "cjs-package",
+        buildEnd: async () => {
+            await mkdir(`dist/${src}/cjs`, { recursive: true });
+            await writeFile(`./dist/${src}/cjs/package.json`, JSON.stringify(pkg, null, 2));
+        },
+    };
+}
+
+const sources = [
+    "domitory",
+    "eventivity",
+    "appliance",
+    "actribute",
+    "apriori",
+    "generational",
+    "sophistry",
+    "onetomany",
+    "withy",
 ];
+
+const exports = [];
+let extraPlugins;
+for (let [i, src] of sources.entries()) {
+    extraPlugins = (i === 0)? [
+        copy({
+            targets: [{ src: `./package.json`, dest: "dist" }],
+        })
+    ]: [];
+
+    exports.push({
+        input: `./src/${src}.js`,
+        output: [
+            { format: "es", file: `./dist/${src}/esm/${src}.js`},
+            { format: "cjs", file: `./dist/${src}/cjs/${src}.js`},
+        ],
+        plugins: [
+            ...extraPlugins,
+            createPackage(src),
+            createCommonJsPackage(src),
+            terser()
+        ]
+
+    }, {
+        input: `./src/${src}.ts`,
+        plugins: [dts()],
+        output: {
+            format: "es",
+            file: `./dist/${src}/${src}.d.ts`,
+        },
+    })
+}
+
+export default exports;
+
