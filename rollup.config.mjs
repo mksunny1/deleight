f46f3f77ab8d43d26526462a7c2adc1326f2a8a1
markersunny <166430764+markersunny@users.noreply.github.com>
2024-09-07 22:15:04 +0100

    Removed unnecessary dev dependencies

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,8 +1,5 @@
 import dts from "rollup-plugin-dts";
-import copy from "rollup-plugin-copy";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
-// import terser from "@rollup/plugin-terser";
-// import typescript from '@rollup/plugin-typescript';
 
 import { writeFile, mkdir } from "fs/promises";
 
